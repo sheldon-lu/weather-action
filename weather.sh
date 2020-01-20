a1b85233d10a1db6e65435e6f57c041158144d2f
Sheldon-lu <31846425+sheldon-lu@users.noreply.github.com>
2020-01-20 17:05:48 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -3,7 +3,7 @@
 
 set -eux
 
-CITY=Hangzhou
+CITY=Hangzhou.png
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -11,5 +11,5 @@ UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Ge
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-  -o result.html \
+  -o result.png \
   wttr.in/$CITY
